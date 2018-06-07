e75ab7e4d6a8cb03f1cd19f19ed6496378808732
Nguyen Phi Viet <nguyen.phi.viet@framgia.com>
2018-06-07 11:48:23 +0700

    skip comfirmation when login with google, facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,8 +15,10 @@ class User < ApplicationRecord
     data = access_token.info
     user = User.where(email: data["email"]).first
     unless user
-      user = User.create name: data["name"],
+      user = User.new name: data["name"],
         email: data["email"], password: Devise.friendly_token[0,20]
+      user.skip_confirmation!
+      user.save
     end
     user
   end
