893f86fe0b311f5af37776c7bd30905501b3cd8f
Nguyen Phi Viet <nguyen.phi.viet@framgia.com>
2018-06-08 15:09:00 +0700

    show contact, send message and fix to skip confirmation email when login google, facebook

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root "home#index"
+  get "/contact", to: "home#contact"
+  post "/contact", to: "home#send_contact"
   devise_for :users, path: "", path_names: {sign_in: "login",
     sign_out: "logout", sign_up: "signup", edit: "profile"},
     controllers: {omniauth_callbacks: "omniauth_callbacks"}
